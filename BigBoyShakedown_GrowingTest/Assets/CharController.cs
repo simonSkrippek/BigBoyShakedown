62de0cce50c6ffef87be12677de87cbc438dd77a
Simon Skrippek <simon.skrippek@t-online.de>
2019-12-08 18:07:15 +0100

    Added movement based on character size
    non physics base dmovement implemented. acceleration and deceleration is simulated, for the sake of custom modifiability.
    play around with the variables in the editor AFTER YOU HAVE READ THE TOOLTIPS

--- BigBoyShakedown_GrowingTest/Assets/CharController.cs
+++ BigBoyShakedown_GrowingTest/Assets/CharController.cs
@@ -18,25 +18,32 @@ public class CharController : MonoBehaviour
 
     #region movement
     [Header("character size")]
-    [Tooltip("minimum size of character")]
-    [SerializeField] private int minimumCharacterSize;
+    [Tooltip("minimum size of character; CANNOT BE ZERO")]
+    [SerializeField] private int minimumCharacterSize; 
     [Tooltip("maximum size of character")]
     [SerializeField] private int maximumCharacterSize;
     [Tooltip("current size of character; DO NOT CHANGE")]
-    [SerializeField] private int characterSize;
+    [SerializeField] private int characterSize = 1;
+    private bool sizeChanged = false;
 
     private Vector2 currentMovement;
     private Dictionary<Vector2, float> previousDirections;
     private float timeAcceleratingInDirection;
-    private float maxAccelerationTime;
+    [Header("acceleration time")]
+    [Tooltip("maximum time counted towards acceleration")]
+    [SerializeField] private float maxAccelerationTime;
     [Header("movement speed")]
-    [Tooltip("")]
-    [SerializeField] private int moveSpeed;
+    [Tooltip("base movement speed")]
+    [SerializeField] private float moveSpeed;
+    [Tooltip("scaling variable for pastMovements")]
+    [SerializeField] private float pastMovementStrength;
+    [Tooltip("scaling variable for currentMovements")]
+    [SerializeField] private float currentMovementStrength;
 
 
     #endregion
 
-    void Start()
+    void Awake()
     {
         cameraForward = cam.transform.forward; // Set forward to equal the camera's forward vector
         cameraForward.y = 0; // make sure y is 0
@@ -44,7 +51,9 @@ public class CharController : MonoBehaviour
         cameraRight = Quaternion.Euler(new Vector3(0, 90, 0)) * cameraForward; // set the right-facing vector to be facing right relative to the camera's forward vector
 
         previousDirections = new Dictionary<Vector2, float>();
+        characterSize = 1;
     }
+
 
     void Update()
     {
@@ -61,49 +70,104 @@ public class CharController : MonoBehaviour
         }
         else
         {
-            previousDirections.Add(currentMovement, characterSize);
+            if (currentMovement != Vector2.zero) AddToPreviousMovements(currentMovement, timeAcceleratingInDirection);
             currentMovement = newCurrentMovement;
+            timeAcceleratingInDirection = Time.deltaTime;
         }
 
         if (Input.mouseScrollDelta.y != 0) ChangeCharacterSize(Input.mouseScrollDelta.y);
     }
+    private void AddToPreviousMovements(Vector2 direction, float time)
+    {
+        float timeIfAlreadyInDic = 0;
+        if (previousDirections.TryGetValue(direction, out timeIfAlreadyInDic))
+        {
+            time += timeIfAlreadyInDic;
+            previousDirections.Remove(direction);
+        }
+        previousDirections.Add(direction, time);
+    }
     private void ChangeCharacterSize(float y)
     {
-        if (y > 0 && characterSize < maximumCharacterSize) characterSize++;        
-        else if (characterSize > minimumCharacterSize) characterSize--;        
+        if (y > 0 && characterSize < maximumCharacterSize)
+        {
+            characterSize++;
+            sizeChanged = true;
+        }
+        else if (y < 0 && characterSize > minimumCharacterSize)
+        {
+            characterSize--;
+            sizeChanged = true;
+        }
     }
 
     private void FixedUpdate()
     {
+        ApplySize();
         ApplyMovement();
+    }
+    private void ApplySize()
+    {
+        if (sizeChanged)
+        {
+            var position = new Vector3(this.transform.position.x, characterSize / 2f + .3f, this.transform.position.z);
+            this.transform.localScale = new Vector3(characterSize, characterSize, characterSize);
+            this.transform.position = position;
+            sizeChanged = false;
+
+            maxAccelerationTime = characterSize;
+            previousDirections.Clear();
+            currentMovement = Vector2.zero;
+            timeAcceleratingInDirection = 0f;
+        }
     }
     private void ApplyMovement()
     {
-        Vector2 pastMovement = new Vector2();
-        foreach (var keyValuePair in previousDirections)
+        Vector2 currentMovementUnscaled = new Vector2();
+        if (currentMovement != Vector2.zero)
         {
-            Vector2 direction = keyValuePair.Key;
-            float time = keyValuePair.Value;
+            currentMovementUnscaled = currentMovement * timeAcceleratingInDirection;
+        }
+        Vector2 pastMovementUnscaled = new Vector2();
+        if (previousDirections != null && previousDirections.Count != 0)
+        {
+            foreach (var keyValuePair in previousDirections)
+            {
+                Vector2 direction = keyValuePair.Key.normalized;
+                float time = keyValuePair.Value;
+
+                pastMovementUnscaled += (direction * time);
+            }
         }
 
-        Vector3 rightMovement = cameraRight;
-        Vector3 upMovement = cameraForward;
+        Vector2 pastMovementScaled = pastMovementUnscaled * moveSpeed * pastMovementStrength * Time.fixedDeltaTime;
+        Vector2 currentMovementScaled = currentMovementUnscaled * moveSpeed * currentMovementStrength / characterSize * Time.fixedDeltaTime;
+        Vector2 allMovement = pastMovementScaled + currentMovementScaled;
 
-        Vector3 heading = rightMovement + upMovement; 
+        Vector3 rightMovement = cameraRight * allMovement.x;
+        Vector3 upMovement = cameraForward * allMovement.y;
 
-        transform.forward = heading.normalized; 
+        Vector3 heading = rightMovement + upMovement;
 
-        transform.position += heading;
+        if (heading != Vector3.zero)
+        {
+            transform.forward = heading.normalized;
+            transform.position += heading;
+        }
+
+        ReduceTimeOnPreviousDirections();
     }
-
-    void Move()
+    private void ReduceTimeOnPreviousDirections()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey")); // setup a direction Vector based on keyboard input. GetAxis returns a value between -1.0 and 1.0. If the A key is pressed, GetAxis(HorizontalKey) will return -1.0. If D is pressed, it will return 1.0
-        Vector3 rightMovement = cameraRight * maxMoveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey"); // Our right movement is based on the right vector, movement speed, and our GetAxis command. We multiply by Time.deltaTime to make the movement smooth.
-        Vector3 upMovement = cameraForward * maxMoveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey"); // Up movement uses the forward vector, movement speed, and the vertical axis inputs.
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement); // This creates our new direction. By combining our right and forward movements and normalizing them, we create a new vector that points in the appropriate direction with a length no greater than 1.0
-        transform.forward = heading; // Sets forward direction of our game object to whatever direction we're moving in
-        transform.position += rightMovement; // move our transform's position right/left
-        transform.position += upMovement; // Move our transform's position up/down
+        Dictionary<Vector2, float> newTimeDic = new Dictionary<Vector2, float>();
+        foreach (var keyValuePair in previousDirections)
+        {
+            Vector2 direction = keyValuePair.Key.normalized;
+            float time = keyValuePair.Value - Time.fixedDeltaTime;
+            ///(characterSize/2)
+
+            if (time > 0) newTimeDic.Add(direction, time);
+        }
+        previousDirections = newTimeDic;
     }
 }
