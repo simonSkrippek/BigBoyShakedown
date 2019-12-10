13e08c14856cafa3a9a0f356feffefc050405664
Simon Skrippek <simon.skrippek@t-online.de>
2019-12-10 20:05:29 +0100

    changed graph to be not blocky

--- BigBoyShakedown_BillboardTest/Assets/scripts/Billboard.cs
+++ BigBoyShakedown_BillboardTest/Assets/scripts/Billboard.cs
@@ -6,10 +6,17 @@ public class Billboard : MonoBehaviour
 {
     // Start is called before the first frame update
     public float score;
+    float changeValue;
     [SerializeField] LineRenderer line;
     List<Vector3> linePositions;
     bool scoreChanged = false;
     [SerializeField] scoreZone[] scoreZones;
+
+    bool changeInProgress = false;
+    int framesChangeInProgress = 0;
+    [Header("change length")][Tooltip("time score changes in frames")]
+    [SerializeField] int scoreChangeLength = 3;
+    
 
     void Start()
     {
@@ -25,27 +32,40 @@ public class Billboard : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
+        if (linePositions.Count <= 1)
+        {
+            var v = new Vector3(GetTime(), score, 0);
+            linePositions.Add(v);
+            linePositions.Add(v);
+            line.positionCount = linePositions.Count;
+
+        }
+
         if (scoreChanged)
         {
-            linePositions.Add(new Vector3(GetTime(), score, 0));
+            if (!changeInProgress) linePositions.Add(new Vector3(GetTime(), score, 0));
             linePositions.Add(new Vector3(GetTime(), score, 0));
             line.positionCount = linePositions.Count;
             scoreChanged = false;
+            Debug.Log("score no longer changed");
         }
-        else
+        if (changeInProgress)
         {
-            if (linePositions.Count > 1)
+            linePositions[linePositions.Count - 1] = new Vector3(GetTime(), score - (changeValue / (float)scoreChangeLength * ((float)scoreChangeLength - (float)framesChangeInProgress++)), 0);
+            if (framesChangeInProgress == scoreChangeLength)
             {
-                linePositions[linePositions.Count - 1] = new Vector3(GetTime(), score, 0);
-            }
-            else
-            {
-                var v = new Vector3(GetTime(), score, 0);
-                linePositions.Add(v);
-                linePositions.Add(v);
+                changeInProgress = false;
+                Debug.Log("change no longer in progress");
+                linePositions.Add(new Vector3(GetTime(), score, 0));
                 line.positionCount = linePositions.Count;
             }
         }
+        if (!changeInProgress && !scoreChanged)
+        {
+
+            linePositions[linePositions.Count - 1] = new Vector3(GetTime(), score, 0);
+        }
+
         line.SetPositions(linePositions.ToArray());
     }
 
@@ -56,7 +76,19 @@ public class Billboard : MonoBehaviour
 
     public void ScoreChangedHandler(float scoreChange)
     {
+        if (changeInProgress)
+        {
+            linePositions.Add(new Vector2(GetTime(), score));
+        }
+
         scoreChanged = true;
+        Debug.Log("score changed");
+
         score += scoreChange;
+        changeValue = scoreChange;
+
+        changeInProgress = true;
+        Debug.Log("change in progress");
+        framesChangeInProgress = 0;
     }
 }
